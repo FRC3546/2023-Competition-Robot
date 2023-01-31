59cd709dc2e7d457914340ef35ebb77ea678d7a4
James <jfriggs1877@gmail.com>
2023-01-30 21:08:04 -0500

    Created a Flipper subsystem. It used a double solenoid for the clamping part and a BAG motor for rotating it up and down. WARNING: THE BAG MOTOR NEEDS A WAY TO STOP WHEN IMPLEMENTING INTO THE ROBOT. (MAYBE LIMIT SWITCH) Thanks to Jake for letting me know what the flipper looks like right now.

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -31,17 +31,14 @@ public class ExampleSubsystem extends SubsystemBase {
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
   public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
     return false;
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
   }
 
   @Override
   public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
---
+++ src/main/java/frc/robot/subsystems/FlipperSubsystem.java
@@ -0,0 +1,67 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+
+public class FlipperSubsystem extends SubsystemBase{
+    
+    private DoubleSolenoid flipperClampSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 3);
+    private boolean flipperClampOpen = false;
+
+    private VictorSP raiseFlipperMotor = new VictorSP(0);
+    private boolean flipperArmRaised = false;
+    
+
+
+    public void OpenFlipperClamp(){
+        flipperClampSolenoid.set(Value.kReverse);
+        flipperClampOpen = true;
+    }
+
+    public void CloseFlipperClamp(){
+        flipperClampSolenoid.set(Value.kForward);
+        flipperClampOpen = false;
+    }
+
+    public void ToggleFlipperClamp(){
+
+        if(flipperClampOpen == true){
+            flipperClampOpen = false;
+        }
+        else if(flipperClampOpen == false){
+            flipperClampOpen = true;
+        }
+
+        flipperClampSolenoid.toggle();
+    }
+
+    // NEEDS A SYSTEM TO STOP THE MOTOR!!! (limit switch or something)
+    public void RaiseFlipperArm(){
+        raiseFlipperMotor.set(0.5);
+        flipperArmRaised = true;
+    }
+
+    public void LowerFlipperArm(){
+        raiseFlipperMotor.set(-0.5);
+        flipperArmRaised = false;
+    }
+
+    public void toggleFlipperArm(){
+        
+        if(flipperArmRaised == true){
+            flipperArmRaised = false;
+            LowerFlipperArm();
+        }
+
+        if(flipperArmRaised == false){
+            flipperArmRaised = true;
+            RaiseFlipperArm();
+        }
+
+    }
+
+}
