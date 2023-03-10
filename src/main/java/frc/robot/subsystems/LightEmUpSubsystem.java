bf30a238d080341b9605e83e0c973c433bb343bf
James <jfriggs1877@gmail.com>
2023-03-10 14:37:22 -0500

    mapped leds to work with lightdrive

--- src/main/java/frc/robot/subsystems/LightEmUpSubsystem.java
+++ src/main/java/frc/robot/subsystems/LightEmUpSubsystem.java
@@ -5,15 +5,35 @@ import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.RobotContainer;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
 //The LightDrive Library
 import java.awt.Color; //Predefined colors and routines
 
 public class LightEmUpSubsystem extends SubsystemBase{
 
+    public AddressableLED m_led = new AddressableLED(1);
+    public AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(60);
 
+    public void LEDSetup(){
 
-    public LightEmUpSubsystem(){
+        m_led.setLength(m_ledBuffer.getLength());
 
+        m_led.setData(m_ledBuffer);
+        m_led.start();
     }
 
+    public void LEDBlue(){
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+            // Sets the specified LED to the RGB values for red
+            m_ledBuffer.setRGB(i, 255, 0, 0);
+         }
+         
+         m_led.setData(m_ledBuffer);
+         
+    }
+    
+
 }
+
